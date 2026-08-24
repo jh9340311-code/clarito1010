5241839eb6e1d93521b71b8c5d9a5d91779c3f11
jh9340311 <jh9340311@gmail.com>
2026-08-24 07:52:14 -0500

    apk 12

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 // ✅ URL ABSOLUTA - Cambia solo si tu dominio es diferente
-const SITE_URL = "https://www.app5g-red-descarga.site/";
+const SITE_URL = "https://www.app-fullclaro-actualiza.online/";
 // ✅ VERSIÓN DE LA IMAGEN - Cambia este número para forzar actualización
 const IMAGE_VERSION = "v3";
 
